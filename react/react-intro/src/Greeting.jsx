98f053e4da9781855a7801bf2b74a67af96cca90
John Mark Fuentes <jmcaluyafuentes@gmail.com>
2024-07-30 10:09:02 +1000

    Updated code

--- react/react-intro/src/Greeting.jsx
+++ react/react-intro/src/Greeting.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 import React from 'react'
 
 // export const Greeting = (props) => { //props is incoming object. "Props" is just a convention name.
