e8efa2950b7d960194468408a8a7d85feab1c35e
John Mark Fuentes <jmcaluyafuentes@gmail.com>
2024-08-03 11:56:43 +1000

    Combine two sorted arrays into one

---
+++ practice/algorithm/string.js
@@ -0,0 +1,11 @@
+// function substring(string, startIndex, endIndex) {
+
+// }
+
+const string = 'acbdef'; // length = 6
+const startIndex = 4; // 4
+const endIndex = 6; // 6
+
+const newString = string.substring(startIndex, endIndex);
+
+console.log(newString); // 'ef'
