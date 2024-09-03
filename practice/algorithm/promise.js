9608f06add87e3eb0eac56b4edc518aa826065fd
John Mark Fuentes <jmcaluyafuentes@gmail.com>
2024-09-03 16:24:43 +1000

    Async and await

--- practice/algorithm/promise.js
+++ practice/algorithm/promise.js
@@ -1,17 +1,38 @@
-const fetch = () => {
-    return new Promise((res, rej) => {
-        if(true) {
-            res('Fetching Success')
-        } else {
-            rej('Fetching Rejected')
-        }
-    })
-}
+// const fetch = async () => {
+//     return new Promise((res, rej) => {
+//         if(true) {
+//             res('Fetching Success')
+//         } else {
+//             rej('Fetching Rejected')
+//         }
+//     })
+// }
 
-fetch()
-    .then((res) => {
-        console.log(res)
-    })
-    .catch((rej) => {
-        console.log(rej)
-    })
+// fetch()
+//     .then((res) => {
+//         console.log(res)
+//     })
+//     .catch((rej) => {
+//         console.log(rej)
+//     })
+
+const fetchData = async () => {
+
+    try {
+        const response = await fetch('https://api.example.com/data');
+
+        if (!response.ok) {
+            throw new Error(`Fetching unsuccessful: ${response.status}`)
+        }
+        console.log('Fetched data: ', response);
+        
+    } catch (error) {
+        if (error.name === "TypeError") {
+            console.error('Network error occurred: ', error.message);
+        } else {
+            console.error('Error: ', error.message);
+        }
+    }
+};
+
+fetchData();
